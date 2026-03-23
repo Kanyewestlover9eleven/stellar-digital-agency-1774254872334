bac6087095efe98b600a66501a9277967e0dbe74
DatDawg <149214606+Kanyewestlover9eleven@users.noreply.github.com>
2026-03-23 16:34:40 +0800

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: '/stellar-digital-agency-1774254872334',
+  assetPrefix: '/stellar-digital-agency-1774254872334'
+};
+module.exports = nextConfig;
